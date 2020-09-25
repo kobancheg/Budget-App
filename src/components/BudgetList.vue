13b7a7079de3bf96393f8e03ce726d52426a24ba
kobancheg <aleks.revati@gmail.com>
2020-09-25 12:28:58 +0300

    make component budget list

---
+++ src/components/BudgetList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="budget-list-wrap">
+    <ElCard header="header"> </ElCard>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BudgetList",
+  props: {
+    list: {
+      type: Object,
+      default: () => ({}),
+    },
+  },
+  data: () => ({
+    header: "Budget List",
+  }),
+};
+</script>
+
+<style scoped>
+</style>
---
+++ src/plugins/elements/index.js
@@ -0,0 +1,12 @@
+import Vue from 'vue';
+import lang from 'element-ui/lib/locale/lang/en';
+import locale from 'element-ui/lib/locale';
+import 'element-ui/packages/theme-chalk/lib/index.css';
+import { Button, Card, Form, FormItem, Input, Select } from 'element-ui';
+
+const elements = [Button, Card, Form, FormItem, Input, Select];
+
+locale.use(lang);
+
+elements.forEach((El) => Vue.use(El, { locale }));
+
