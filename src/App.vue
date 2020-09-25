84625f724a1182fdf100c14b4db219594d8e4caa
kobancheg <aleks.revati@gmail.com>
2020-09-25 17:18:01 +0300

    add component forms

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div id="app">
+    <Form />
     <TotalBalance :total="totalBalance" />
     <BudgetList :list="list" @deleteItem="onDeleteItem" />
   </div>
@@ -8,12 +9,14 @@
 <script>
 import BudgetList from '@/components/BudgetList';
 import TotalBalance from '@/components/TotalBalance';
+import Form from '@/components/Form';
 
 export default {
   name: 'App',
   components: {
     BudgetList,
     TotalBalance,
+    Form,
   },
   data: () => ({
     list: {
---
+++ src/components/Form.vue
@@ -0,0 +1,50 @@
+<template>
+  <ElCard class="form-card">
+    <ElForm :model="formData">
+      <ElFormItem label="Type" prop="type">
+        <ElSelect
+          class="type-select"
+          v-model="formData.type"
+          placeholder="Choose type..."
+        >
+          <ElOption lable="Income" value="INCOME" />
+          <ElOption lable="Outcome" value="OUTCOME" />
+        </ElSelect>
+      </ElFormItem>
+      <ElFormItem label="Comments" prop="comment">
+        <ElInput v-model="formData.comment" />
+      </ElFormItem>
+      <ElFormItem label="Value" prop="value">
+        <ElInput v-model.number="formData.value" />
+      </ElFormItem>
+      <ElButton @click="onSubmit" type="primary">Submit</ElButton>
+    </ElForm>
+  </ElCard>
+</template>
+
+<script>
+export default {
+  name: 'Form',
+  data: () => ({
+    formData: {
+      type: 'INCOME',
+      comment: '',
+      value: 0,
+    },
+  }),
+  methods: {
+    onSubmit() {},
+  },
+};
+</script>
+
+<style>
+.form-card {
+  max-width: 500px;
+  margin: auto;
+}
+
+.type-select {
+  width: 100%;
+}
+</style>
