1d0b6d8f766aad624b669dd27fa504b8f96405df
kobancheg <aleks.revati@gmail.com>
2020-09-29 18:07:09 +0300

    custom validation

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -30,30 +30,35 @@
 <script>
 export default {
   name: "Form",
-  data: () => ({
-    formData: {
-      type: "INCOME",
-      comment: "",
-      value: 0,
-    },
-    rules: {
-      type: [
-        { required: true, message: "Please select type", trigger: "blur" },
-      ],
-      comment: [
-        { required: true, message: "Please input comment", trigger: "change" },
-      ],
-      value: [
-        { required: true, message: "Please input value", trigger: "change" },
-        {
-          type: "number",
-          message: "Value must be a number",
-          trigger: "change",
-        },
-      ],
-    },
-  }),
-
+  data() {
+    let checkValue = (rule, value, callback) => {
+      if (!value || value === 0) {
+        return callback(new Error("Please input number not equal to 0"));
+      } else {
+        callback();
+      }
+    };
+    return {
+      formData: {
+        type: "INCOME",
+        comment: "",
+        value: 0,
+      },
+      rules: {
+        type: [
+          { required: true, message: "Please select type", trigger: "blur" },
+        ],
+        comment: [
+          {
+            required: true,
+            message: "Please input comment",
+            trigger: "change",
+          },
+        ],
+        value: [{ validator: checkValue, trigger: "change" }],
+      },
+    };
+  },
   methods: {
     onSubmit() {
       this.$refs.addItemForm.validate((valid) => {
